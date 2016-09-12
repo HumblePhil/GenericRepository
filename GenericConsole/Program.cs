1bfecfd6cd92208bf76ca616920b8291de8d1c1c
OCTOPUS10\Phil <phil.li@udngroup.com.tw>
2016-09-12 14:32:16 +0800

    Initial commit.

---
+++ GenericConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
